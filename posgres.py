07610a3558139f39010aad23d41fa5f0ed1f073e
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-08-26 01:56:03 -0700

    fixed working source

--- posgres.py
+++ posgres.py
@@ -2,7 +2,7 @@
 
 import psycopg2
 
-conn = psycopg2.connect(host="localhost", database="postgres", user="postgres")
+conn = psycopg2.connect(host="127.0.0.1", database="suppliers", user="postgres", password="uppercut100")
 
 if conn is not None:
     print("Connection established to PostgreSQL.")
